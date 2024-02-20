ae0b025ca2af693b7af196bae874a06d1ea7717f
narcissusniki <160579376+narcissusniki@users.noreply.github.com>
2024-02-20 12:33:48 +0330

    controller.js

--- src/routes/auth/controller.js
+++ src/routes/auth/controller.js
@@ -1,6 +1,6 @@
 const controller = require('./../controller');
 const _ = require('lodash');
-const bcrypt = require('bcrypt');  //رمزنگاری 
+const bcrypt = require('bcrypt');  //encryption 
 const config = require('config');
 const jwt = require('jsonwebtoken');
 
@@ -9,16 +9,13 @@ module.exports = new (class extends controller {
     let user = await this.User.findOne({email: req.body.email});
     if(user){
       return this.response({
-        res, code:400, message: 'this user already registered'   //با قرار دادن کلوشه باز و بسته نیاز به ترتیب نیست
+        res, code:400, message: 'this user already registered'  
       });
     }
-    // const {email, name, password} = req.body;
-    // user = new this.User({email, name, password});
     user = new this.User(_.pick(req.body, ["name", "email", "password"]));
 
     const salt = await bcrypt.genSalt(10);
     user.password = await bcrypt.hash(user.password, salt);
-      //قبل ذخیره رمز در دیتابیس اون رو رمز نگتری میکنن - با تکنیک هش که یک تکنیک رمزنگاری یک طرفست که برای هیچ دو ورودی متفاوت خروجی یکسان نداره
   
     await user.save();
 
